5ac092016feb8616be906648b966b8f7879594c0
Rafael Menezes <rafaelmenezes86@gmail.com>
2018-02-03 09:33:53 -0200

    Created model and migration usuario

---
+++ app/Usuario.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    protected $table = 'usuario';
+
+    protected $fillable = ['nome', 'sobrenome', 'email', 'telefone', 'funcao', 'status', 'senha'];
+
+    protected $dates = ['deleted_at'];
+}
---
+++ database/migrations/2018_02_01_234254_create_usuario_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsuarioTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nome', 100);
+            $table->string('sobrenome', 100);
+            $table->string('email', 100);
+            $table->string('telefone', 50);
+            $table->string('funcao', 30);
+            $table->string('status', 30);
+            $table->string('senha', 255);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
