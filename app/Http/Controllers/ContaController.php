204b1d41b4d42b603b722c4cad1ba0616f4fabd2
Rafael Menezes <rafaelmenezes86@gmail.com>
2018-02-14 23:56:54 -0200

    Created controllers Conta and Oportunidade

--- app/Http/Controllers/ContaController.php
+++ app/Http/Controllers/ContaController.php
@@ -6,79 +6,22 @@ use Illuminate\Http\Request;
 
 class ContaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
+  public function index()
+  {
+      $contas = Conta::with(['usuario', 'cidade'])->get();
+      return response()->json($contas);
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  public function show($id)
+  {
+      $conta = Conta::with(['usuario', 'cidade'])->find($id);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
+      if (!$conta) {
+          return response()->json([
+              'message' => 'Record not found'
+          ], 404);
+      }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+      return response()->json($conta);
+  }
 }
---
+++ app/Http/Controllers/OportunidadeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OportunidadeController extends Controller
+{
+    public function index()
+    {
+        $oportunidades = Oportunidade::with(['fasevenda', 'conta'])->get();
+        return response()->json($oportunidades);
+    }
+
+    public function show($id)
+    {
+        $oportunidade = Oportunidade::with(['fasevenda', 'conta'])->find($id);
+
+        if (!$oportunidade) {
+            return response()->json([
+                'message' => 'Record not found'
+            ], 404);
+        }
+
+        return response()->json($oportunidade);
+    }
+
+    public function store(Request $request)
+    {
+        $oportunidade = new Oportunidade();
+        $oportunidade->fill($request->all());
+        $oportunidade->save();
+
+        return response()->json($oportunidade, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $oportunidade = Oportunidade::find($id);
+
+        if(!$oportunidade) {
+            return response()->json([
+                'message'   => 'Record not found',
+            ], 404);
+        }
+
+        $oportunidade->fill($request->all());
+        $oportunidade->save();
+
+        return response()->json($oportunidade);
+    }
+
+    public function destroy($id)
+    {
+        $oportunidade = Oportunidade::find($id);
+
+        if (!$oportunidade) {
+            return response()->json([
+                'message'   => 'Record not found',
+            ], 404);
+        }
+
+        $oportunidade->delete();
+    }
+}
